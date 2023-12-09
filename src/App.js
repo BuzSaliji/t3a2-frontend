bbeef2bcd72a5f580ff1b7d778c1dc6baa6ddfa2
Burim Saliji <buz.saliji@gmail.com>
2023-12-10 07:47:47 +1100

    Add footer to routes Create Footer css file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
+import Footer from './components/footer/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />    
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />    
+        </Routes>
+      <Footer />
     </Router>
   );
 }
