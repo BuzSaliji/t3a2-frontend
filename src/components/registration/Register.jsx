db032a390d3e87221d199f63a606d889af39605a
Burim Saliji <buz.saliji@gmail.com>
2023-12-10 11:52:23 +1100

    Create Register function

---
+++ src/components/registration/Register.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './Register.css'; // Create and link a CSS file for styling
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+    };
+
+    return (
+        <div className="register-container">
+            <h1>Register</h1>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
