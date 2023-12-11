3cf2579376a47d1b696485f8c9af02fc110e63f8
Burim Saliji <buz.saliji@gmail.com>
2023-12-11 20:49:20 +1100

    Update Logic to get time availability per court

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -5,62 +5,113 @@ import './Calendar.css';
 
 function CalendarComponent() {
     const [selectedDate, setSelectedDate] = useState(new Date());
-    const [court1TimeSlots, setCourt1TimeSlots] = useState([]);
-    const [court2TimeSlots, setCourt2TimeSlots] = useState([]);
+    const [court1Bookings, setCourt1Bookings] = useState([]);
+    const [court2Bookings, setCourt2Bookings] = useState([]);
+    // eslint-disable-next-line no-unused-vars
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState(null);
+    // eslint-disable-next-line no-unused-vars
+    const [selectedCourt, setSelectedCourt] = useState(null);
 
     useEffect(() => {
-        fetchTimeSlots(selectedDate);
+        fetchBookings(selectedDate);
     }, [selectedDate]);
 
     const onChange = (newDate) => {
         setSelectedDate(newDate);
+        fetchBookings(newDate);
     };
 
-    const fetchTimeSlots = async (date) => {
+    const fetchBookings = async (date) => {
         const formattedDate = date.toISOString().split('T')[0];
-    
+        const court1Id = '65768b76da0ab5cec28c9f33'; 
+        const court2Id = '65768b76da0ab5cec28c9f34'; 
+
         try {
-            // Fetch time slots for Court 1
-            const responseCourt1 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/timeslots/court1?date=${formattedDate}`);
+            // Fetch bookings for Court 1
+            const responseCourt1 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookings/by-court?courtId=${court1Id}&startDate=${formattedDate}&endDate=${formattedDate}`);
             const dataCourt1 = await responseCourt1.json();
-            setCourt1TimeSlots(dataCourt1);
-    
-            // Fetch time slots for Court 2
-            const responseCourt2 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/timeslots/court2?date=${formattedDate}`);
+            setCourt1Bookings(dataCourt1);
+
+            // Fetch bookings for Court 2
+            const responseCourt2 = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookings/by-court?courtId=${court2Id}&startDate=${formattedDate}&endDate=${formattedDate}`);
             const dataCourt2 = await responseCourt2.json();
-            setCourt2TimeSlots(dataCourt2);
+            setCourt2Bookings(dataCourt2);
         } catch (error) {
-            console.error('Error fetching time slots:', error);
+            console.error('Error fetching bookings:', error);
         }
+    };
+
+    const calculateAvailableTimeSlots = (bookings) => {
+        const operatingHours = { start: 9, end: 20 }; // 9:00 AM to 8:00 PM
+        const timeSlots = [];
+
+        // Generate all possible time slots for the day
+        for (let hour = operatingHours.start; hour < operatingHours.end; hour++) {
+            const slotStart = new Date(selectedDate);
+            slotStart.setHours(hour, 0, 0, 0);
+            const slotEnd = new Date(slotStart);
+            slotEnd.setHours(hour + 1, 0, 0, 0);
+
+            // Check if the slot overlaps with any existing bookings
+            const isAvailable = !bookings.some(booking => {
+                const bookingStart = new Date(booking.timeSlot.start);
+                const bookingEnd = new Date(booking.timeSlot.end);
+                return (slotStart < bookingEnd && slotEnd > bookingStart);
+            });
+
+            if (isAvailable) {
+                timeSlots.push(`${hour.toString().padStart(2, '0')}:00`);
+            }
+        }
+
+        return timeSlots;
     };
 
     const handleTimeSlotSelection = (slot, courtNumber) => {
         console.log(`Selected time slot for Court ${courtNumber}:`, slot);
+        setSelectedTimeSlot(slot);
+        setSelectedCourt(courtNumber);
+
+        // Additional actions, such as opening a modal, navigating to a booking page, etc.
+        // For example, you might want to set a state to open a modal where the user can confirm their booking
+        // setBookingModalOpen(true);
     };
 
+    const court1TimeSlots = calculateAvailableTimeSlots(court1Bookings);
+    const court2TimeSlots = calculateAvailableTimeSlots(court2Bookings);
+
     return (
-        <div className="calendar-container">
-            <Calendar onChange={onChange} value={selectedDate} />
-            <div className="court-time-slots">
-                <h3>Court 1</h3>
-                <div className="time-slots">
-                    {court1TimeSlots.map((slot, index) => (
-                        <button key={index} onClick={() => handleTimeSlotSelection(slot, 1)}>
-                            {slot}
-                        </button>
-                    ))}
+        <div className="main-container">
+            <div className="calendar-container">
+                <Calendar onChange={onChange} value={selectedDate} />
+            </div>
+    
+            <div className="court-time-slots-container">
+                <div className="court-container">
+                    <h3>Court 1</h3>
+                    <div className="time-slots">
+                        {court1TimeSlots.map((slot, index) => (
+                            <button key={index} onClick={() => handleTimeSlotSelection(slot, 1)}>
+                                {slot}
+                            </button>
+                        ))}
+                    </div>
                 </div>
-                <h3>Court 2</h3>
-                <div className="time-slots">
-                    {court2TimeSlots.map((slot, index) => (
-                        <button key={index} onClick={() => handleTimeSlotSelection(slot, 2)}>
-                            {slot}
-                        </button>
-                    ))}
+    
+                <div className="court-container">
+                    <h3>Court 2</h3>
+                    <div className="time-slots">
+                        {court2TimeSlots.map((slot, index) => (
+                            <button key={index} onClick={() => handleTimeSlotSelection(slot, 2)}>
+                                {slot}
+                            </button>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
     );
-};
+}
 
 export default CalendarComponent;
+
