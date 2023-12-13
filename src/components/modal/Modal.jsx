5980be24c40d8e30103d0d2eed046008be5e83ea
Burim Saliji <buz.saliji@gmail.com>
2023-12-14 05:27:13 +1100

    Add logic to pass booking to the backend

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Modal.css'; 
 
 const Modal = ({ selectedBooking, onClose }) => {
     const navigate = useNavigate(); 
+    const [error, setError] = useState('');
 
-    const handleBookingConfirmation = () => {
-        // Here you can add logic to send booking data to backend
-        // For now, let's navigate to a confirmation page
-        navigate('/booking-confirmation', { 
-            state: { 
-                date: selectedBooking.date, 
-                time: selectedBooking.time, 
-                courtNumber: selectedBooking.courtNumber 
+    const handleBookingConfirmation = async () => {
+        const token = localStorage.getItem('token'); 
+        const userId = localStorage.getItem('userId');
+        const courtId = selectedBooking.courtNumber; 
+    
+        const startTime = new Date(selectedBooking.date);
+        const [hours, minutes] = selectedBooking.time.split(':').map(Number);
+        startTime.setHours(hours, minutes, 0, 0);
+    
+        const endTime = new Date(startTime);
+        endTime.setHours(endTime.getHours() + 1); // Assuming 1 hour duration
+    
+        try {
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookings`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                    courtId: courtId,
+                    date: selectedBooking.date,
+                    timeSlot: {
+                        start: startTime,
+                        end: endTime
+                    }
+                })
+            });
+    
+            if (!response.ok) {
+                throw new Error('Failed to confirm booking. Please try again.');
             }
-        });
-        onClose();
+    
+            const responseData = await response.json();
+            console.log('Booking confirmed:', responseData);
+    
+            
+            navigate('/booking-confirmation', { 
+                state: { 
+                    bookingDetails: responseData 
+                }
+            });
+    
+            onClose(); 
+        } catch (error) {
+            console.error('Error confirming booking:', error);
+            setError(error.message);
+        }
     };
 
     return (
@@ -24,6 +63,7 @@ const Modal = ({ selectedBooking, onClose }) => {
                 <span className="close" onClick={onClose}>&times;</span>
                 <h2>Confirm Your Booking</h2>
                 <p>Court {selectedBooking.courtNumber} at {selectedBooking.time}</p>
+                {error && <div className="error-message">{error}</div>}
                 <button onClick={handleBookingConfirmation}>Confirm Booking</button>
             </div>
         </div>
