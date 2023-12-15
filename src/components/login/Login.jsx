931c8c93dc9c1e80960432278c68976e81cf66fe
Burim Saliji <buz.saliji@gmail.com>
2023-12-15 20:45:39 +1100

    Modal confirmation success

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,70 +1,77 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
-
 import logo from '../../assets/images/Black logo - no background.png';
-
 import './Login.css';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { login } = useContext(UserContext);
-  const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useContext(UserContext);
+    const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      console.log("Attempting to log in with:", { email, password });
+  
+      try {
+          const loginUrl = `${process.env.REACT_APP_BACKEND_URL}/users/login`;
+          console.log("Sending request to:", loginUrl);
+          const response = await fetch(loginUrl, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ email, password }),
+          });
+  
+          console.log("Response status:", response.status);
+  
+          if (!response.ok) {
+              const errorResponse = await response.json();
+              console.error("Response data (not ok):", errorResponse);
+              throw new Error('Login failed');
+          }
+  
+          const data = await response.json();
+          console.log('Login successful, response data:', data);
+  
+          if (data.jwt && data.userId) {
+              // Store user ID in local storage
+              localStorage.setItem('userId', data.userId);
+  
+              // Use the login method from the context after the response is successful
+              login(data.user, data.jwt);
+              navigate('/calendar');
+          } else {
+              throw new Error('JWT or User ID not received');
+          }
+      } catch (error) {
+          console.error('Login error:', error);
+      }
+  };
+  
 
-    try {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
-
-        if (!response.ok) {
-            throw new Error('Login failed');
-        }
-
-        const data = await response.json();
-        console.log('Response data:', data);
-
-        // Use the login method from the context after the response is successful
-        login(data.user, data.jwt);
-
-        // Navigate to the calendar page
-        navigate('/calendar');
-    } catch (error) {
-        console.error('Login error:', error);
-    }
-};
-
-  return (
-    
-    <div className="login-container">
-      <div className="logo-container">
-      <img src={logo} alt="Court Connect Logo" className="logo" />
-
-      </div>
-      <h1>Login</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email Address</label>
-          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    return (
+        <div className="login-container">
+            <div className="logo-container">
+                <img src={logo} alt="Court Connect Logo" className="logo" />
+            </div>
+            <h1>Login</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" value={password} autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit">Login</button>
+                <p className="signup-link">Not registered? <Link to="/register">Sign up!</Link></p>
+            </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" value={password} autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
-        <p className="signup-link">Not registered? <Link to="/register">Sign up!</Link></p>
-      </form>
-    </div>
-  );
+    );
 }
 
-
 export default Login;
-
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -5,54 +5,82 @@ import './Modal.css';
 
 const Modal = ({ onClose }) => {
     const { selectedBooking } = useContext(BookingContext);
-    // console.log("Modal time:", selectedBooking ? selectedBooking.time : "No booking");
     const navigate = useNavigate(); 
     const [error, setError] = useState('');
+
+    const getCourtObjectId = (courtNumber) => {
+        const courtIds = {
+            1: '65768b76da0ab5cec28c9f33', 
+            2: '65768b76da0ab5cec28c9f34'
+        };
+        return courtIds[courtNumber] || null;
+    };
 
     const handleBookingConfirmation = async () => {
         const token = localStorage.getItem('token'); 
         const userId = localStorage.getItem('userId');
+
+
+        if (!userId) {
+            console.error("User ID is not set");
+            setError("User not identified. Please log in again.");
+            return;
+        }
+
         const courtId = selectedBooking.courtNumber; 
-    
+        if (!courtId) {
+            console.error("Court ID is not set");
+            setError("Court not selected. Please select a court.");
+            return;
+        }
+
         const startTime = new Date(selectedBooking.date);
         const [hours, minutes] = selectedBooking.time.split(':').map(Number);
         startTime.setHours(hours, minutes, 0, 0);
-    
+
         const endTime = new Date(startTime);
         endTime.setHours(endTime.getHours() + 1); 
-    
+
+        const startTimeISO = startTime.toISOString();
+        const endTimeISO = endTime.toISOString();
+        const dateISO = selectedBooking.date.toISOString();
+
+        const courtObjectId = getCourtObjectId(selectedBooking.courtNumber);
+
         try {
+            const requestBody = {
+                userId: userId,
+                courtId: courtObjectId, 
+                date: dateISO,
+                startTime: startTimeISO,
+                endTime: endTimeISO
+            };
+            
+            console.log('Sending booking request:', requestBody);
+    
             const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/bookings`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify({
-                    userId: userId,
-                    courtId: courtId,
-                    date: selectedBooking.date,
-                    timeSlot: {
-                        start: startTime,
-                        end: endTime
-                    }
-                })
+                body: JSON.stringify(requestBody)
             });
     
             if (!response.ok) {
+                const errorResponse = await response.json();
+                console.error('Server responded with an error:', errorResponse);
                 throw new Error('Failed to confirm booking. Please try again.');
             }
-    
+
             const responseData = await response.json();
             console.log('Booking confirmed:', responseData);
-    
-            
             navigate('/booking-confirmation', { 
                 state: { 
                     bookingDetails: responseData 
                 }
             });
-    
+
             onClose(); 
         } catch (error) {
             console.error('Error confirming booking:', error);
