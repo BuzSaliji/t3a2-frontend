e8fb2987ec7b117fd64a18f696388a6e66955db2
Burim Saliji <buz.saliji@gmail.com>
2023-12-21 16:36:22 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <Router>
         <Navbar />
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} /> 
             <Route path="/calendar" element={<CalendarComponent />} /> 
             <Route path="/confirmation/:bookingId" element={<Confirmation />} />
